d7b6e61d6541d33f87efa849d84f55723d2f0b90
Tania Ludueña <Tanialuduenaok@gmail.com>
2023-10-21 16:03:08 -0300

    Grabo cambios de 3er Desafio

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+const ProductManager = require('../ProductManager');
+
+const productManager = new ProductManager('products.json');
+
+app.use(express.json());
+
+// Ruta para obtener todos los productos
+app.get('/products', async (req, res) => {
+    try {
+        const limit = req.query.limit;
+        const products = await productManager.getProducts(limit);
+        res.json(products);
+    } catch (error) {
+        res.status(500).json({ error: 'Error al obtener productos' });
+    }
+});
+
+// Ruta para obtener un producto por su ID
+app.get('/products/:pid', async (req, res) => {
+    const productId = parseInt(req.params.pid);
+    const product = await productManager.getProductById(productId);
+
+    if (product) {
+        res.json(product);
+    } else {
+        res.status(404).json({ error: 'Producto no encontrado' });
+    }
+});
+
+app.listen(8080, () => {
+    console.log(`Servidor Express escuchando en el puerto 8080`);
+});
+
